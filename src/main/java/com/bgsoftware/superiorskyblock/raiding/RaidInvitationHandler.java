eb502a0bef4ac20b7254c3b7d8f52b0dd4387695
Tre Logan <tr3.logan@gmail.com>
2021-03-09 15:42:53 -0500

    Finish invitation system

--- src/main/java/com/bgsoftware/superiorskyblock/raiding/RaidInvitationHandler.java
+++ src/main/java/com/bgsoftware/superiorskyblock/raiding/RaidInvitationHandler.java
@@ -1,12 +1,63 @@
 package com.bgsoftware.superiorskyblock.raiding;
 
+import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 public class RaidInvitationHandler {
+
     private static Set<RaidInvitation> raidInvitations = new HashSet<>();
+
+    static {
+        Bukkit.getScheduler().runTaskTimer(SuperiorSkyblockPlugin.getPlugin(), () -> {
+            Iterator<RaidInvitation> iterator = raidInvitations.iterator();
+            while (iterator.hasNext()) {
+                RaidInvitation invitation = iterator.next();
+                invitation.setTimeLeft(invitation.getTimeLeft() - 1);
+                SuperiorSkyblockPlugin.raidDebug("Invitation from " + invitation.getSenderUuid() + " has " + invitation.getTimeLeft() + " time left.");
+                if (invitation.getTimeLeft() <= 0) {
+                    Player invitee = Bukkit.getPlayer(invitation.getInviteeUuid());
+                    Player sender = Bukkit.getPlayer(invitation.getSenderUuid());
+                    if (invitee != null && sender != null) {
+                        invitee.sendMessage("Invitation from " + sender.getName() + " expired.");
+                        sender.sendMessage("Invitation to " + invitee.getName() + " expired.");
+                    }
+                    iterator.remove();
+                    SuperiorSkyblockPlugin.raidDebug("Invitation by " + invitation.getSenderUuid() + " to " + invitation.getInviteeUuid() + " expired.");
+                }
+            }
+        }, 20, 20);
+    }
 
     public static boolean addInvitation(RaidInvitation invitation) {
         return raidInvitations.add(invitation);
+    }
+
+    public static boolean containsInvitationTo(UUID uuid) {
+        return raidInvitations.stream().anyMatch(invitation -> invitation.getInviteeUuid().equals(uuid));
+    }
+
+    public static RaidInvitation getInvitation(UUID sender, UUID invitee) {
+        return raidInvitations.stream()
+                .filter(invitation -> invitation.getSenderUuid().equals(sender) && invitation.getInviteeUuid().equals(invitee))
+                .findAny()
+                .get();
+    }
+
+    public static boolean removeInvitation(UUID sender, UUID invitee) {
+        return raidInvitations.removeIf(invitation -> invitation.getSenderUuid().equals(sender) && invitation.getInviteeUuid().equals(invitee));
+    }
+
+    public static boolean removeInvitationsOfSender(UUID sender) {
+        return raidInvitations.removeIf(invitation -> invitation.getSenderUuid().equals(sender));
+    }
+
+    public static boolean removeInvitationsOfInvitee(UUID invitee) {
+        return raidInvitations.removeIf(invitation -> invitation.getInviteeUuid().equals(invitee));
     }
 }
--- src/main/java/com/bgsoftware/superiorskyblock/raiding/command/CmdRaid.java
+++ src/main/java/com/bgsoftware/superiorskyblock/raiding/command/CmdRaid.java
@@ -2,9 +2,11 @@ package com.bgsoftware.superiorskyblock.raiding.command;
 
 import com.bgsoftware.superiorskyblock.Locale;
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
+import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.commands.ISuperiorCommand;
 import com.bgsoftware.superiorskyblock.raiding.RaidInvitation;
 import com.bgsoftware.superiorskyblock.raiding.RaidInvitationHandler;
+import com.bgsoftware.superiorskyblock.raiding.SuperiorRaid;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -16,6 +18,7 @@ import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 public final class CmdRaid implements ISuperiorCommand {
 
@@ -46,7 +49,7 @@ public final class CmdRaid implements ISuperiorCommand {
 
     @Override
     public int getMaxArgs() {
-        return 2;
+        return 3;
     }
 
     @Override
@@ -56,34 +59,77 @@ public final class CmdRaid implements ISuperiorCommand {
 
     @Override
     public void execute(SuperiorSkyblockPlugin plugin, CommandSender sender, String[] args) {
-        String inviteeName = args[1];
-        Player invitee = Bukkit.getPlayer(inviteeName);
-
+        String arg = args[1];
+        Player commandSender = (Player) sender;
+        Player invitee = Bukkit.getPlayer(arg);
         if (invitee == null) {
-            sender.sendMessage("Couldn't find player with name " + inviteeName + ".");
+            if (args.length == 3) {
+                Player invitationSender = Bukkit.getPlayer(args[2]);
+                if (arg.equalsIgnoreCase("accept")) acceptInvitation(invitationSender, commandSender, plugin);
+                else if (arg.equalsIgnoreCase("deny")) declineInvitation(invitationSender, commandSender);
+                else sender.sendMessage("Couldn't find player with name " + arg + ".");
+            } else commandSender.sendMessage("Invalid number of arguments.");
+        } else {
+            RaidInvitation invitation = new RaidInvitation(commandSender.getUniqueId(), invitee.getUniqueId());
+            if (RaidInvitationHandler.addInvitation(invitation)) sender.sendMessage("Invitation sent.");
+            else sender.sendMessage("You've already sent an invitation to this player.");
+
+            TextComponent[] messageComponents = createInvitationMessageComponents(sender.getName());
+            invitee.spigot().sendMessage(messageComponents);
+        }
+    }
+
+    private void acceptInvitation(Player invitationSender, Player commandSender, SuperiorSkyblockPlugin plugin) {
+        RaidInvitation invitation = handleInvitation(invitationSender.getUniqueId(), commandSender.getUniqueId());
+
+        if (invitation == null) {
+            commandSender.sendMessage("You have no pending invitations from this player.");
             return;
         }
 
-        RaidInvitation invitation = new RaidInvitation(((Player) sender).getUniqueId(), invitee.getUniqueId());
-        RaidInvitationHandler.addInvitation(invitation);
-        sender.sendMessage("Invitation sent.");
+        Island teamOneIsland = plugin.getGrid().getIsland(invitation.getSenderUuid());
+        Island teamTwoIsland = plugin.getGrid().getIsland(invitation.getInviteeUuid());
 
-        TextComponent[] messageComponents = createInvitationMessageComponents(sender.getName(), inviteeName);
-        invitee.spigot().sendMessage(messageComponents);
+        SuperiorRaid raid = new SuperiorRaid(teamOneIsland.getIslandMembers(true), teamTwoIsland.getIslandMembers(true));
+        raid.startRaid();
     }
 
-    private TextComponent[] createInvitationMessageComponents(String senderName, String inviteeName) {
-        TextComponent mainComponent = new TextComponent(senderName + " has invited you to a raid.");
-        mainComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("This text is shown on hover!").create()));
+    private void declineInvitation(Player invitationSender, Player commandSender) {
+        if (RaidInvitationHandler.removeInvitation(invitationSender.getUniqueId(), commandSender.getUniqueId())) {
+            commandSender.sendMessage("Invitation declined.");
+            invitationSender.sendMessage(commandSender.getName() + " has declined your invitation.");
+        } else {
+            commandSender.sendMessage("You have no pending invitations from this player.");
+        }
+    }
+
+    private RaidInvitation handleInvitation(UUID sender, UUID invitee) {
+        RaidInvitation invitation = RaidInvitationHandler.getInvitation(sender, invitee);
+        if (invitation == null) return null;
+        RaidInvitationHandler.removeInvitationsOfSender(sender);
+        RaidInvitationHandler.removeInvitationsOfInvitee(invitee);
+        return invitation;
+    }
+
+    private TextComponent[] createInvitationMessageComponents(String senderName) {
+        TextComponent mainComponent = new TextComponent(senderName + " has invited you to a raid. ");
+        mainComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("[Insert island level and members online here]").create()));
 
         TextComponent optionAccept = new TextComponent("Accept");
-        optionAccept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/island startraid " + senderName + " " + inviteeName));
+        optionAccept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/island raid accept " + senderName));
         optionAccept.setColor(ChatColor.GREEN);
 
+        TextComponent orText = new TextComponent(" or ");
+        orText.setColor(ChatColor.WHITE);
+
         TextComponent optionDecline = new TextComponent("Decline");
+        optionDecline.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/island raid deny " + senderName));
         optionDecline.setColor(ChatColor.RED);
 
-        return new TextComponent[]{mainComponent, optionAccept, optionDecline};
+        TextComponent period = new TextComponent(".");
+        period.setColor(ChatColor.WHITE);
+
+        return new TextComponent[]{mainComponent, optionAccept, orText, optionDecline};
     }
 
     @Override
